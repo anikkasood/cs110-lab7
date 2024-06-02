a930b6a49c8ba6e4fb47ad2240e76d7cfa618711
Kelsey Musolf <kelsey@musolf.net>
2024-06-01 21:24:32 -0700

    attempting the get room stuff in controller room.js

--- lab7/server.js
+++ lab7/server.js
@@ -36,20 +36,52 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
 });
+
+
 // set up stylesheets route
 
 // TODO: Add server side code
 
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri,  {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// }
+// );
 
 //connecting to local host mongodb - KM
 //const uri = "mongodb+srv://kelseymoose346:laMaENwsdzG46M2D@lab7.ziuefqs.mongodb.net/?retryWrites=true&w=majority&appName=lab7";
 // mongoose.connect("mongodb+srv://kelseymoose346:laMaENwsdzG46M2D@lab7.ziuefqs.mongodb.net/?retryWrites=true&w=majority&appName=lab7");
 
+async function run() {
+    try {
+      // Connect the client to the server (optional starting in v4.7)
+      await client.connect();
+  
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 // Create controller handlers to handle requests at each endpoint
 app.get('/', homeHandler.getHome);
-app.get('/:roomName', roomHandler.getRoom);
+//app.get('/:roomName', roomHandler.getRoom);
+
+app.use(roomHandler.router);
+//app.post('/create', roomHandler.createRoom); //as
+//app.get("/:roomName", roomHandler.getRoom); KM - need to add
+//app.get('/:roomName/messages', roomHandler.getRoom);    KM - need to add
 
 // NOTE: This is the sample server.js code we provided, feel free to change the structures
 
